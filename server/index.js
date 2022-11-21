447db43b538fabd9ee91f13d3214b308774bee6b
Henry Bryant <htbryant1256@eagle.fgcu.edu>
2022-11-20 22:39:28 -0500

    Removed underline from links

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.get("/api", (req, res) => {
     res.json({
         "naples_locations": [["Baker Park", 
             "1433 2nd Ave S, Naples, FL 34102",
-            "description" : "Baker Park is a large park lined mostly with concrete pathways. Easily skateable, lots of areas for flatground tricks."],
+            "Baker Park is a large park lined mostly with concrete pathways. Easily skateable, lots of areas for flatground tricks."],
             ["Naples Test Location 2:"]],
         "ft_myers_locations": ["Fort Myers Test Location 1\n:", "Fort Myers Test Location 2:"]    });
 
